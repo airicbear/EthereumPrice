6a469bd556996fd17125e342a1674f02e157effb
Eric Nguyen <tuk94307@temple.edu>
2021-05-26 21:00:23 -0400

    Add refresh functionality to widget

--- app/src/main/java/com/github/airicbear/ethereumprice/EthereumPriceAppWidget.kt
+++ app/src/main/java/com/github/airicbear/ethereumprice/EthereumPriceAppWidget.kt
@@ -1,10 +1,15 @@
 package com.github.airicbear.ethereumprice
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.util.Log
 import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
 import com.github.airicbear.ethereumprice.model.EtherPriceResult
 import com.github.airicbear.ethereumprice.network.EtherApi
 import retrofit2.Call
@@ -27,12 +32,25 @@ class EthereumPriceAppWidget : AppWidgetProvider() {
         }
     }
 
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
-    }
+    override fun onReceive(context: Context, intent: Intent) {
+        val views = RemoteViews(context.packageName, R.layout.ethereum_price_app_widget)
+        views.setTextViewText(R.id.appwidget_text, context.getString(R.string.appwidget_text))
+        widgetFetchEtherPrice(context, views)
 
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
+        // Useful log to track intents for the App Widget
+        Log.d("EtherPrice", "App Widget received intent ${intent.action}")
+
+        if (intent.action == AppWidgetManager.ACTION_APPWIDGET_UPDATE) {
+
+            // Setup on click
+            //
+            // For some reason, apparently onReceive() is the only function being called?
+            // Which is why we put this here instead of in, onUpdate(), for example
+            val intentUpdate = Intent(context, EthereumPriceAppWidget::class.java)
+            intentUpdate.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+            val pendingUpdate = PendingIntent.getBroadcast(context, 0, intentUpdate, PendingIntent.FLAG_IMMUTABLE)
+            views.setOnClickPendingIntent(R.id.appwidget_text, pendingUpdate)
+        }
     }
 }
 
@@ -43,18 +61,20 @@ internal fun updateAppWidget(
 ) {
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.ethereum_price_app_widget)
-    widgetFetchEtherPrice(context, appWidgetManager, appWidgetId, views)
+
+    // Update views
+    widgetFetchEtherPrice(context, views)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
 
-internal fun widgetFetchEtherPrice(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int, views: RemoteViews) {
+internal fun widgetFetchEtherPrice(context: Context, views: RemoteViews) {
     EtherApi.retrofitService.getEtherPrice(BuildConfig.ETHER_SCAN_API_KEY).enqueue(object :
         Callback<EtherPriceResult> {
         override fun onFailure(call: Call<EtherPriceResult>, t: Throwable) {
             views.setTextViewText(R.id.appwidget_text, context.getString(R.string.connection_error))
-            appWidgetManager.updateAppWidget(appWidgetId, views)
+            AppWidgetManager.getInstance(context).updateAppWidget(ComponentName(context, EthereumPriceAppWidget::class.java), views)
             Log.d("EtherPrice", call.request().toString())
             Log.d("EtherPrice", t.toString())
         }
@@ -64,7 +84,7 @@ internal fun widgetFetchEtherPrice(context: Context, appWidgetManager: AppWidget
             val ethUsdValue = etherResponse?.result?.usdValue?.toDouble()
             val ethValue = NumberFormat.getCurrencyInstance().format(ethUsdValue)
             views.setTextViewText(R.id.appwidget_text, ethValue)
-            appWidgetManager.updateAppWidget(appWidgetId, views)
+            AppWidgetManager.getInstance(context).updateAppWidget(ComponentName(context, EthereumPriceAppWidget::class.java), views)
             Log.d("EtherPrice", "ETH Value: $ethValue")
         }
     })
